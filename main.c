2908e4a2adb7f42c358e2f222a5c99941447ff02
Mike Pontillo <mpontillo@spectrum42.com>
2019-04-08 16:09:14 -0700

    main.c: Remove comment indicating how to compile. Add getline() example.

--- main.c
+++ main.c
@@ -1,9 +1,22 @@
-// cc [-Wall] [-Werror] [-std=c11] -o main main.c
+// See also: https://www.gnu.org/software/libc/manual/html_node/Feature-Test-Macros.html
+#define _POSIX_C_SOURCE 200809L
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char* argv[])
 {
-    printf("Hello, world!\n");
+    char* line = NULL;
+    size_t buffer_size = 0;
+    ssize_t count;
+
+    printf("What is your name?\n");
+    count = getline(&line, &buffer_size, stdin);
+    printf("Allocated %zu characters.\n", buffer_size);
+    printf("Read %zu characters.\n", count);
+    line[count-1] = '\0';
+    printf("Hello, %s!\n", line);
+    free(line);
+
     return 0;
 }
