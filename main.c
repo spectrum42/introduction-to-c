9fc862cdeb7aa92171fc18334dd890c0f7a5c788
Mike Pontillo <mpontillo@spectrum42.com>
2019-04-15 18:07:10 -0700

    Add 'playing' loop.

--- main.c
+++ main.c
@@ -1,5 +1,9 @@
+#define _POSIX_C_SOURCE 200809L
+
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <strings.h>
 
 #include "input.h"
 #include "player.h"
@@ -8,12 +12,22 @@ int main(int argc, char* argv[])
 {
     char* line;
     struct player* player = NULL;
+    bool playing = true;
 
     printf("What is your name?\n");
     printf("> ");
     line = input_getline();
     player = player_new(line);
     player_print(player);
+    while(playing) {
+        printf("> ");
+        line = input_getline();
+        if(strcasecmp("quit", line) == 0) {
+            playing = false;
+        } else {
+            printf("Unknown command. Type \"quit\" to exit the program.\n");
+        }
+    }
     player_free(player);
 
     return 0;
